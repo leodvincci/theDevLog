75fb3632381b7abe278ba297b08d464e50530cff
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 <lpenrose@depaul.edu>
2023-02-01 00:16:04 -0600

    added cross origin configs, connected userPrompt to api

--- DevLog_Backend/src/main/java/com/example/devlog_backend/Controller/UserPromptController.java
+++ DevLog_Backend/src/main/java/com/example/devlog_backend/Controller/UserPromptController.java
@@ -3,10 +3,7 @@ package com.example.devlog_backend.Controller;
 import com.example.devlog_backend.Entity.UserPrompt;
 import com.example.devlog_backend.Service.UserPromptService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1")
@@ -15,6 +12,7 @@ public class UserPromptController {
     @Autowired
     UserPromptService userPromptService;
 
+    @CrossOrigin
     @PostMapping("/saveUserPrompt")
     public void saveUserPrompt(@RequestBody UserPrompt userPrompt){
         userPromptService.saveUserPrompt(userPrompt.getId(),userPrompt.getDays(),userPrompt.getMonths(),userPrompt.getYears(),userPrompt.getTitle(),userPrompt.getPromptValue());
